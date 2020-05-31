65f39354ceee6d76ab3bce98d5ab70ec5c958c95
Sina Booeshaghi <sbooeshaghi@gmail.com>
2020-05-31 11:35:40 -0700

    Fixed cli bugs, added a bunch of todo items

--- bcltools/bcltools.py
+++ bcltools/bcltools.py
@@ -1,21 +1,17 @@
-from struct import *
+import struct
 import sys
 import gzip
 import os
-import collections
-import itertools
 
-from .utils import (
-    prepend_zeros_to_number
-)
+from .utils import (prepend_zeros_to_number)
 
 # Byte specification of *.bcl
 # Note: N is the cluster index
-# 
+#
 # Bytes         | Description              | Data type
 # -----------------------------------------------------
 # Bytes 0–3     | Number N of cluster      | Unsigned 32bits little endian integer
-# 
+#
 # Bytes 4–(N+3) | Bits 0-1 are the bases,  | Unsigned 8bits integer
 #               | respectively [A, C, G, T]
 #               | for [0, 1, 2, 3]: bits
@@ -26,13 +22,14 @@ from .utils import (
 #               | for no-call.
 
 
-
 def get_n_reads(fastq):
-    """Count the number of lines in a FASTQ file and divide by 4. Returns the number of reads"""
+    """Count the number of lines in a FASTQ file and divide by 4.
+    Returns the number of reads"""
     with gzip.open(fastq, 'rb') as f:
         for lidx, l in enumerate(f, 1):
             pass
-        return lidx//4
+        return lidx // 4
+
 
 def get_read_len(fastq):
     """Get the length of a single FASTQ record"""
@@ -41,23 +38,26 @@ def get_read_len(fastq):
         seq = f.readline().strip()
         return len(seq)
 
+
 # TODO this is needed for Miseq
 # def make_bcl_folders(read_len, path):
 #     base_path = os.path.join(path, "BaseCalls/L001/")
 #     if not os.path.exists(base_path):
 #         for i in range(read_len):
 #             os.makedirs(os.path.join(base_path, f"C{i+1}.1"))
-#     return 
+#     return
+
 
 # TODO: split this function in half. First half initializes folders
 # Second half initializes the files
 def make_bcl_files(path, read_len, n_reads):
     """
-    Create initialized bcl files. There are read_len number of bcl files. Each has a header corresponding to the number of reads.
+    Create initialized bcl files. There are read_len number of bcl files.
+    Each has a header corresponding to the number of reads.
     """
     bcls = []
     header_fmt = "<i"
-    header = pack(header_fmt, n_reads)
+    header = struct.pack(header_fmt, n_reads)
 
     base_path = os.path.join(path, "BaseCalls/L001/")
     if not os.path.exists(base_path):
@@ -71,23 +71,25 @@ def make_bcl_files(path, read_len, n_reads):
     return bcls
 
 
+base2num = {"A": 0, "C": 1, "G": 2, "T": 3}
 
-base2num = {"A":0, "C":1, "G":2, "T":3}
+
 def write_bcl_record(base, qual, bcl):
     """
-    Write a single record containing a quality score and a base, to the bcl file. 
+    Write a single record containing a quality score and a base, to the bcl file.
     """
     seq_fmt = "<B"
 
     r = 0
     if base != "N":
-        q = (ord(qual)-33) << 2
+        q = (ord(qual) - 33) << 2
         b = base2num[base]
         r = q | b
-    record = pack(seq_fmt, r)
+    record = struct.pack(seq_fmt, r)
     with open(bcl, 'ab') as f:
         f.write(record)
     return
+
 
 def write_seqs_quals(fastq, bcls):
     """
@@ -100,26 +102,24 @@ def write_seqs_quals(fastq, bcls):
 
         for lidx, l in enumerate(f, 0):
             if lidx % 4 == 0:
-                seq  = l.strip().decode()
+                seq = l.strip().decode()
                 seq_bool = True
 
-            elif (lidx+2)%4==0:
+            elif (lidx + 2) % 4 == 0:
                 qual = l.strip().decode()
                 qual_bool = True
-            
+
             if seq_bool and qual_bool:
-                seq_bool = False; qual_bool = False
+                seq_bool = False
+                qual_bool = False
 
                 for idx, (b, q) in enumerate(zip(seq, qual)):
                     write_bcl_record(b, q, bcls[idx])
 
-    return 
+    return
 
 
 def fastq2bcl(path, fastq):
-    header_fmt = "<i"
-    seq_fmt = "<B"
-
     read_len = get_read_len(fastq)
     n_reads = get_n_reads(fastq)
     # make_bcl_folders(read_len, path) # not needed for nextseq
@@ -127,19 +127,20 @@ def fastq2bcl(path, fastq):
     write_seqs_quals(fastq, bcls)
 
 
-num2base = {0:"A", 1:"C", 2:"G", 3:"T"}
+num2base = {0: "A", 1: "C", 2: "G", 3: "T"}
+
 
 def bcl2fastq(file_path):
     header_fmt = "<i"
     seq_fmt = "<B"
 
     with open(file_path, "br") as f:
-        up = unpack(header_fmt, f.read(4))
+        up = struct.unpack(header_fmt, f.read(4))
         print(up)
-        itr = iter_unpack(seq_fmt, f.read())
+        itr = struct.iter_unpack(seq_fmt, f.read())
         for idx, i in enumerate(itr):
             base = num2base.get(3 & i[0], 0)
-            qual = i[0]>>2
-            print(bin(i[0]),"\t", base, "\t", qual)
+            qual = i[0] >> 2
+            sys.stdout.write(f'{bin(i[0])}\t{base}\t{qual}\n')
 
     return
--- bcltools/main.py
+++ bcltools/main.py
@@ -1,24 +1,102 @@
 #!/usr/bin/env python3
 
-from struct import *
-import sys
 import argparse
-from .bcltools import (
-    bcl2fastq,
-    fastq2bcl
-)
+import sys
+from .bcltools import (bcl2fastq, fastq2bcl)
+
+
+def parse_b2f(args):
+    return bcl2fastq(args.bcl)
+
+
+def parse_f2b(args):
+    # TODO check if -o exists or not, make path accordingly
+
+    return fastq2bcl(args.o, args.fastq)
+
+
+def setup_b2f_args(parser, parent):
+    parser_b2f = parser.add_parser(
+        'b2f',
+        description='Convert bcl files to fastq files',
+        help='Convert bcl files to fastq files',
+        parents=[parent]
+    )
+
+    parser_b2f.add_argument(
+        '-o',
+        metavar='OUT FOLDER',
+        help='output folder',
+        type=str,
+        required=True
+    )
+
+    # currently takes only one, add support for more than one
+    parser_b2f.add_argument('bcl')
+
+    # TODO add slicing functionality for the read
+    # TODO add option for the type of machine
+
+    return parser_b2f
+
+
+def setup_f2b_args(parser, parent):
+    parser_f2b = parser.add_parser(
+        'f2b',
+        description='Convert fastq files to bcl files',
+        help='Convert fastq files to bcl files',
+        parents=[parent]
+    )
+
+    parser_f2b.add_argument(
+        '-o',
+        metavar='OUT FOLDER',
+        help='output folder',
+        type=str,
+        required=True
+    )
+    # currently takes only one, add support for more than one
+    parser_f2b.add_argument('fastq')
+    # TODO add option for the type of machine
+
+    return parser_f2b
+
+
+COMMAND_TO_FUNCTION = {'f2b': parse_f2b, 'b2f': parse_b2f}
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', metavar="OUT", help='output folder', type=str, required=True)
-    parser.add_argument('file')
+
+    subparsers = parser.add_subparsers(
+        dest='command',
+        metavar='<CMD>',
+    )
+
+    parent = argparse.ArgumentParser(add_help=False)
+
+    parser_b2f = setup_b2f_args(subparsers, parent)
+    parser_f2b = setup_f2b_args(subparsers, parent)
+
+    command_to_parser = {'f2b': parser_f2b, 'b2f': parser_b2f}
+
+    # Show help when no arguments are given
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    if len(sys.argv) == 2:
+        if sys.argv[1] in command_to_parser:
+            command_to_parser[sys.argv[1]].print_help(sys.stderr)
+        else:
+            parser.print_help(sys.stderr)
+        sys.exit(1)
+
     args = parser.parse_args()
 
-    bcl2fastq(args.file)
-    # fastq2bcl(args.o, args.file)
+    COMMAND_TO_FUNCTION[args.command](args)
 
     return 1
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
